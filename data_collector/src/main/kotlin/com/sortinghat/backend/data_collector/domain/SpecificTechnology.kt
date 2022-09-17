4ee01623e65e2eaaef2d72ee68e724937c963865
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2022-09-17 17:16:30 -0300

    remove useless UUID
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- data_collector/src/main/kotlin/com/sortinghat/backend/data_collector/domain/SpecificTechnology.kt
+++ data_collector/src/main/kotlin/com/sortinghat/backend/data_collector/domain/SpecificTechnology.kt
@@ -1,7 +1,3 @@
 package com.sortinghat.backend.data_collector.domain
 
-import java.util.UUID
-
-abstract class SpecificTechnology {
-    val id = UUID.randomUUID().toString()
-}
+abstract class SpecificTechnology
