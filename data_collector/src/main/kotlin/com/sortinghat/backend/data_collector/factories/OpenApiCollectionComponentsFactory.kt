d251877b4d086e6f279627da6647cb330fd8c58b
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2022-09-19 02:03:51 -0300

    add open api factory
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- data_collector/src/main/kotlin/com/sortinghat/backend/data_collector/factories/OpenApiCollectionComponentsFactory.kt
+++ data_collector/src/main/kotlin/com/sortinghat/backend/data_collector/factories/OpenApiCollectionComponentsFactory.kt
@@ -1,25 +1,25 @@
 package com.sortinghat.backend.data_collector.factories
 
 import com.sortinghat.backend.data_collector.services.ConverterToDomain
-import com.sortinghat.backend.data_collector.services.DockerComposeToDomain
 import com.sortinghat.backend.data_collector.services.FetchDataFromRemoteRepository
-import com.sortinghat.backend.data_collector.services.ParseDockerCompose
+import com.sortinghat.backend.data_collector.services.OpenApiToDomain
+import com.sortinghat.backend.data_collector.services.ParseOpenApi
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 
 @Component
-@Qualifier("dockerCompose")
-class DockerComposeCollectionComponentsFactory(
+@Qualifier("openApi")
+class OpenApiCollectionComponentsFactory(
     @Autowired private val fetcher: FetchDataFromRemoteRepository,
-    @Autowired private val parser: ParseDockerCompose,
-    @Autowired private val converter: DockerComposeToDomain,
-): CollectionComponentsFactory {
+    @Autowired private val parser: ParseOpenApi,
+    @Autowired private val converter: OpenApiToDomain
+) : CollectionComponentsFactory {
 
     override fun createDataFetcher() = fetcher
 
     override fun createDataParser() = parser
 
     @Suppress("UNCHECKED_CAST")
-    override fun <Service> createConverterToDomain() = converter as ConverterToDomain<Service>
+    override fun <Operation> createConverterToDomain(): ConverterToDomain<Operation> = converter as ConverterToDomain<Operation>
 }
